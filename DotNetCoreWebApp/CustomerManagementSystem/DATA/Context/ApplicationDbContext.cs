40ce96236be1d49ad1041d0227c763e582ade87b
Aioub Amini <aiyoub.amini@outlook.com>
2023-09-09 13:30:14 -0400

    Add project files.

---
+++ DotNetCoreWebApp/CustomerManagementSystem/DATA/Context/ApplicationDbContext.cs
@@ -0,0 +1,32 @@
+﻿using CustomerManagementSystem.DATA.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CustomerManagementSystem.DATA.Context;
+
+public partial class ApplicationDbContext : DbContext
+{
+    private readonly IConfiguration _configuration;
+
+    public ApplicationDbContext()
+    {
+    }
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options, IConfiguration configuration)
+        : base(options)
+    {
+        _configuration = configuration;
+    }
+
+    public DbSet<Customer> Customers { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer(_configuration.GetConnectionString("DefaultConnection"));
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ DotNetCoreWebApp/CustomerManagementSystem/DATA/Entities/Customer.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CustomerManagementSystem.DATA.Entities
+{
+    public class Customer
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [StringLength(256)]
+        public string? FirstName { get; set; }
+
+        [StringLength(256)]
+        public string? LastName { get; set; }
+
+        [Required]
+        [StringLength(256)]
+        public string Email { get; set; }
+    }
+}
